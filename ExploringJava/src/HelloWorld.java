786cb0730f0f277f7da6ca374ce7d3d7f81e5ff2
Majrul Ansari <majrul.ansari@lntinfotech.com>
2020-09-14 12:04:26 +0530

    updated HelloWorld program

--- ExploringJava/src/HelloWorld.java
+++ ExploringJava/src/HelloWorld.java
@@ -1,7 +1,10 @@
+import java.util.Date;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		Date d = new Date();
+		System.out.println(d);
 	}
 }
